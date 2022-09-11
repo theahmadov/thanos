20daacdc5750d8fbd7f065bdd12b4d5f717d6f0d
Error <88781785+TheSadError@users.noreply.github.com>
2022-09-11 10:43:40 +0400

    Update check.py

--- lib_error/check.py
+++ lib_error/check.py
@@ -7,14 +7,14 @@ class error:
     code = {
     100 : ('Could not found <url>...', 'Could not found url to start scan. Please use --u <url> parameter to set url.'),
     101 : ('Try to connect internet...', 'Internet error occured. Try to connect internet and try again.'),
-    102 : ('Url adress could not resolved...', 'Url adress could not resolved. Try to use http:// or https:// methods...')
+    102 : ('Url adress could not resolved...', 'Url adress could not resolved. Try to use http:// or https:// methods , also use correct url.')
     }
 
 class config:
     conf_style = {
         100 : "bold"
     }
-    connect = "1.1.1.1"
+    connect = "https://google.com/"
 
 def print_error(error_code):
     return f"""
@@ -24,10 +24,16 @@ def print_error(error_code):
     """
 
 def check_http(url):
-    if(url.startswith("http") or url.startswith("https")):
+
+    if(url.startswith("http") or url.startswith("https")):            
+        try:
+            connect = requests.get(url)
+        except:
+            return False 
         return True
     else:
         return False
+
 
 def internet():
     try:
@@ -37,8 +43,10 @@ def internet():
     return True
 
 def check_error(url):
+    console.print("Checking internet connection...")
     if(internet() == False):
         console.print(print_error(101), style=config.conf_style[100])
+    console.print("Checking url adress...")
     if(url==None):
         console.print(print_error(100), style=config.conf_style[100])
     if(check_http(url)==False):
