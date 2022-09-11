a7c0e3063919f18a1a7d2d308246403ec7b56a45
Error <88781785+TheSadError@users.noreply.github.com>
2022-09-11 10:43:22 +0400

    Update thanos.py

--- thanos.py
+++ thanos.py
@@ -2,6 +2,7 @@ from rich.console import Console
 from rich.style import Style
 import optparse 
 from lib_error.check import check_error
+from datetime import datetime
 
 console = Console()
 
@@ -10,10 +11,9 @@ class conf:
 
 
 def thanos_main(url):
-    check_error(url)
-
-
-
+    console.print("Thanos started succesfully. Checking some errors. Please wait some time...",style="green")
+    check_report = check_error(url)
+    console.print(f"Starting Thanos scan! Time : {datetime.now()}")
 if __name__ == '__main__':
 
     parser = optparse.OptionParser(f"thanos -u url")
