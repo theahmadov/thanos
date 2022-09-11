11d8cb41956b17cf28580d439aa34ba77fc3a958
Error <88781785+thesaderror@users.noreply.github.com>
2022-09-11 14:34:45 +0400

    Update thanos.py

--- thanos.py
+++ thanos.py
@@ -1,8 +1,10 @@
 from rich.console import Console
 from rich.style import Style
 import optparse 
-from lib_error.check import check_error
 from datetime import datetime
+from scripts.port_scan import thanos_port
+from lib_error.check import check_error
+
 
 console = Console()
 
@@ -10,17 +12,22 @@ class conf:
     bar = ['■', '█','▄','▌','▐']
 
 
-def thanos_main(url):
+def thanos_main(url,params):
     console.print("Thanos started succesfully. Checking some errors. Please wait some time...",style="green")
     check_report = check_error(url)
     console.print(f"Starting Thanos scan! Time : {datetime.now()}")
+    
+    thanos_port(url,"d")
+
 if __name__ == '__main__':
 
     parser = optparse.OptionParser(f"thanos -u url")
-    parser.add_option("-u",dest = "url",type="string") # set url 
+    parser.add_option(" -u",dest = "url",type="string") # set url 
+    parser.add_option("-p",dest = "port",type="string") # set url 
     (options,args) = parser.parse_args()
     
     url = options.url
+    port = options.port
     
     
-    thanos_main(url)
+    thanos_main(url,port)
