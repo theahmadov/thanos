06d71ca0bfc449c2f6b307baabbb88eeee648c52
Error <88781785+thesaderror@users.noreply.github.com>
2022-09-11 14:41:09 +0400

    Deleted -u parameter...
    Deleted -u parameter and now it is more easier to use

--- thanos.py
+++ thanos.py
@@ -2,9 +2,9 @@ from rich.console import Console
 from rich.style import Style
 import optparse 
 from datetime import datetime
-from scripts.port_scan import thanos_port
+from scripts import thanos_port
 from lib_error.check import check_error
-
+import typer
 
 console = Console()
 
@@ -12,22 +12,14 @@ class conf:
     bar = ['■', '█','▄','▌','▐']
 
 
-def thanos_main(url,params):
+def thanos_main(code: str):
     console.print("Thanos started succesfully. Checking some errors. Please wait some time...",style="green")
-    check_report = check_error(url)
+    check_report = check_error(code)
     console.print(f"Starting Thanos scan! Time : {datetime.now()}")
     
-    thanos_port(url,"d")
+    thanos_port.scan(code,"normal") # Port Scan |deep-normal|
 
-if __name__ == '__main__':
 
-    parser = optparse.OptionParser(f"thanos -u url")
-    parser.add_option(" -u",dest = "url",type="string") # set url 
-    parser.add_option("-p",dest = "port",type="string") # set url 
-    (options,args) = parser.parse_args()
-    
-    url = options.url
-    port = options.port
-    
-    
-    thanos_main(url,port)
+
+if __name__ == "__main__":
+    typer.run(thanos_main)
