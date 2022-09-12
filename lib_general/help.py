ea215e0c6b9b88b8b13705ef967899a789b203d8
Error <88781785+thesaderror@users.noreply.github.com>
2022-09-12 21:14:01 +0400

    Update help.py

--- lib_general/help.py
+++ lib_general/help.py
@@ -16,9 +16,21 @@ class config:
 Thanos {version}
 
 -SCAN :
-    port : python3 thanos.py [normal,port]example.com
-    subdomain : python3 thanos.py [normal,sub]example.com
-    directory : python3 thanos.py [normal,dir]example.com
+    -port : python3 thanos.py [normal,port]example.com
+    |
+    |__ Description : Port scanning
+    
+    -subdomain : python3 thanos.py [normal,sub]example.com
+    |
+    |__ Description : Find subdomains of website
+    
+    -directory : python3 thanos.py [normal,dir]example.com
+    |
+    |__ Description : Get all directories of website
+    
+    -record : python3 thanos.py [normal,record]example.com
+    |
+    |__ Description : Record website and extract hidden links
 
 -SAVE :
     save output : python3 thanos.py [normal,port,dir,sub]example.com
