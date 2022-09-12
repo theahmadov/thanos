88c4da89dd501605ef604c2124cb521d338ecf35
Error <88781785+thesaderror@users.noreply.github.com>
2022-09-12 22:20:26 +0400

    Update thanos_rec.py

--- scripts/thanos_rec.py
+++ scripts/thanos_rec.py
@@ -4,6 +4,7 @@ from rich.console import Console
 from rich.style import StyleStack
 from lib_general import ishttp
 import time 
+from sys import platform
 
 console = Console()
 
@@ -24,47 +25,93 @@ def clear_tld(url):
 
 def run(url,save):
     output = ""
-    if(save==False):
-        console.print("\nThanos Report [Record]",style="blue on white")
-        time.sleep(1)
-        console.print("""
-        Recording...
-        [
-            Webdriver : [FireFox]...
-        ]""",style="red")
-        driver = webdriver.Firefox(executable_path='./drivers/geckodriver.exe')
-        driver.get(url)
-        free_tld = clear_tld(clear_http(url))
-        console.print(f"\nExtracted links :\n",style="blue on white")
-        num=1
-        for request in driver.requests:
-            if free_tld in request.url:
-                console.print(f"[{num}] {request.url}",style="blue on yellow")
-                num+=1
+    if(platform == "win32" or platform == "win64"):
+        if(save==False):
+            console.print("\nThanos Report [Record]",style="blue on white")
+            time.sleep(1)
+            console.print("""
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]""",style="red")
+            driver = webdriver.Firefox(executable_path='./drivers/geckodriver.exe')
+            driver.get(url)
+            free_tld = clear_tld(clear_http(url))
+            console.print(f"\nExtracted links :\n",style="blue on white")
+            num=1
+            for request in driver.requests:
+                if free_tld in request.url:
+                    console.print(f"[{num}] {request.url}",style="blue on yellow")
+                    num+=1
 
+        else:
+            console.print("\nThanos Report [Record]",style="blue on white")
+            output += "\nThanos Report [Record]"
+            time.sleep(1)
+            console.print("""
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]""",style="red")
+            output += """\n
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]\n"""
+            driver = webdriver.Firefox(executable_path='./drivers/geckodriver.exe')
+            driver.get(url)
+            free_tld = clear_tld(clear_http(url))
+            console.print(f"\nExtracted links :\n",style="blue on white")
+            output += f"\nExtracted links :\n"
+            num = 1
+            for request in driver.requests:
+                if free_tld in request.url:
+                    console.print(f"[{num}] {request.url}",style="blue on yellow")
+                    output+=f"\n[{num}] {str(request.url)}"
+                    num+=1
+            return output
     else:
-        console.print("\nThanos Report [Record]",style="blue on white")
-        output += "\nThanos Report [Record]"
-        time.sleep(1)
-        console.print("""
-        Recording...
-        [
-            Webdriver : [FireFox]...
-        ]""",style="red")
-        output += """\n
-        Recording...
-        [
-            Webdriver : [FireFox]...
-        ]\n"""
-        driver = webdriver.Firefox(executable_path='./drivers/geckodriver.exe')
-        driver.get(url)
-        free_tld = clear_tld(clear_http(url))
-        console.print(f"\nExtracted links :\n",style="blue on white")
-        output += f"\nExtracted links :\n"
-        num = 1
-        for request in driver.requests:
-            if free_tld in request.url:
-                console.print(f"[{num}] {request.url}",style="blue on yellow")
-                output+=f"\n[{num}] {str(request.url)}"
-                num+=1
-        return output
+        if(save==False):
+            console.print("\nThanos Report [Record]",style="blue on white")
+            time.sleep(1)
+            console.print("""
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]""",style="red")
+            driver = webdriver.Firefox()
+            driver.get(url)
+            free_tld = clear_tld(clear_http(url))
+            console.print(f"\nExtracted links :\n",style="blue on white")
+            num=1
+            for request in driver.requests:
+                if free_tld in request.url:
+                    console.print(f"[{num}] {request.url}",style="blue on yellow")
+                    num+=1
+
+        else:
+            console.print("\nThanos Report [Record]",style="blue on white")
+            output += "\nThanos Report [Record]"
+            time.sleep(1)
+            console.print("""
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]""",style="red")
+            output += """\n
+            Recording...
+            [
+                Webdriver : [FireFox]...
+            ]\n"""
+            driver = webdriver.Firefox()
+            driver.get(url)
+            free_tld = clear_tld(clear_http(url))
+            console.print(f"\nExtracted links :\n",style="blue on white")
+            output += f"\nExtracted links :\n"
+            num = 1
+            for request in driver.requests:
+                if free_tld in request.url:
+                    console.print(f"[{num}] {request.url}",style="blue on yellow")
+                    output+=f"\n[{num}] {str(request.url)}"
+                    num+=1
+            return output
