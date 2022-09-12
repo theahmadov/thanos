8225b2cebfcdce5a322822fb199d04f95bf6a801
Error <88781785+thesaderror@users.noreply.github.com>
2022-09-12 15:00:12 +0400

    Create update.py

---
+++ lib_general/update.py
@@ -0,0 +1,34 @@
+import os
+from rich.console import Console
+from rich.style import Style
+import requests 
+import time 
+
+
+console = Console()
+
+def version():
+    with open("./version.txt","r") as f:
+        version = f.read()
+    return version
+
+
+class conf:
+    bar = ['■', '█','▄','▌','▐']
+
+def new_version():
+    version = requests.get("https://raw.githubusercontent.com/thesaderror/thanos/main/version.txt").text 
+
+def run():
+    console.print(f"Updating Thanos! {version()} --> {new_version}",style="markdown.code")
+    console.print(f"Update module just avaliable on Linux Operating Systems...",style="traceback.exc_type")
+    os.system("cd ..")
+    os.system("rm -rf thanos")
+    os.system("git clone https://github.com/thesaderror/thanos")
+    os.system("pip3 install -r requirements.txt")
+    os.system("clear")
+    for i in range(0,45):
+        console.print(conf.bar[1],style="logging.level.critical",end='')
+        time.sleep(0.1)
+    console.print(f"\nThanos Already Updated! Current Version : {version()}",style="markdown.code")
+    os.system("python3 thanos.py thanos")
